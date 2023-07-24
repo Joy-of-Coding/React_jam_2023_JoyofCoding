8abd256261b93434a05f41819eb7b6636a31b2b1
KatrinaWright <katrina.elise.wright@gmail.com>
2023-07-24 14:28:29 -0400

    Rollback to global dice functions

--- joy-game/src/components/GameZone.tsx
+++ joy-game/src/components/GameZone.tsx
@@ -25,211 +25,120 @@ const GameZone: React.FC<GameZoneProps> = ({game: game, players: players, yourPl
         Rune.actions.rollDice({ nextIndex: nextIndex, numDice: numDice})
     }
 
+
     return (
         <div className='game-play-container'>
             
             <div className="container">
-                <motion.b transition={{ duration: 1.2 }} animate={{y:20}} initial={{y:-150}}   className='player-gameboard-title'>{`${players[yourPlayerId].displayName}'s Game Board`}</motion.b>
-                    
+            <motion.b transition={{ duration: 1.2 }} animate={{y:20}} initial={{y:-150}}   className='player-gameboard-title'>{`${players[yourPlayerId].displayName}'s Game Board`}</motion.b>
                 <div className='top-section'>
 
-                   
+                    <div className="player-section  right">
 
-                        {/* <motion.div  transition={{ duration: 1 }} animate={{x:0}} initial={{x:-150}}   className= { `${playerIds[0] === yourPlayerId ? 'red-border' : ''}player`}>
-                        <div className='player-1-name player-flex'>
+                        <motion.div  transition={{ duration: 1 }} animate={{x:20}} initial={{x:-150}}   className= { `${playerIds[0] === yourPlayerId ? 'red-border' : ''}player`}>
                         <b>   {players[playerIds[0]].displayName} <br/> {game?.diceCount[playerIds[0]]}</b>
-                        </div>                            
-                        
                         </motion.div>
-                    */}
-                        <motion.div
-      transition={{ duration: 1 }}
-      animate={{ x: 0 }}
-      initial={{ x: 150 }}
-      className={`${playerIds[1] === yourPlayerId ? 'red-border' : ''} player-section`}
-    >
-      {numPlayers > 1 ? (
-        <div>
-          <div className='player-2-name player-flex'>
-            <b>{players[playerIds[2]].displayName}: {game?.diceCount[playerIds[2]]}</b>
-          </div>
-        </div>
-      ) : (
-        <div className='player-2-name player-flex'>
-          <b>Waiting for player 2</b>
-        </div>
-      )}
-    </motion.div>
+                    </div>
 
 
-                    
+                    <div className="player-section left">
 
-                        {/* <motion.div transition={{ duration: 1 }} animate={{x:0}} initial={{x:150}}    className={`${playerIds[1] === yourPlayerId ? 'red-border' : ''}player-section`}>
+                        <motion.div transition={{ duration: 1 }} animate={{x:-20}} initial={{x:150}}    className={`top-right player-area grid-item ${playerIds[1] === yourPlayerId ? 'red-border' : ''}player-section`}>
                             {numPlayers > 1 ? (
-
                                 <div>
-                                    
-
-                                <div className='player-2-name player-flex'>
-                                   <b>{players[playerIds[1]].displayName}: {game?.diceCount[playerIds[1]]}</b>
-
+                                    <b>{players[playerIds[1]].displayName}: {game?.diceCount[playerIds[1]]}</b>
                                 </div>
-                                ) : (
-                                <div className='player-2-name player-flex'>
-                                   <b>Waiting for player 2</b> 
+                            ) : (
+                                <div>
+                                    Waiting for player 2
                                 </div>
                             )}
                         </motion.div>
-                     */}
-                    <motion.div
-      transition={{ duration: 1 }}
-      animate={{ x: 0 }}
-      initial={{ x: 150 }}
-      className={`${playerIds[1] === yourPlayerId ? 'red-border' : ''} player-section`}
-    >
-      {numPlayers > 1 ? (
-        <div>
-          <div className='player-2-name player-flex'>
-            <b>{players[playerIds[2]].displayName}: {game?.diceCount[playerIds[2]]}</b>
-          </div>
-        </div>
-      ) : (
-        <div className='player-2-name player-flex'>
-          <b>Waiting for player 2</b>
-        </div>
-      )}
-    </motion.div>
+                    </div>
+                    
 
 
-                </div>      
+                </div>
 
 
                 <div className='middle-section'>
                     
-
-
                     <div className='player-name'>
-                     
+                        {/*Object.keys(players).indexOf(yourPlayerId))*/}
                         {`${players[playerIds[game.currentPlayerIndex]].displayName}'s Turn`}
                         </div>
-                           
-
-               
-                    {/*                     <div className='dice-container'>
-                    {game.gameDice.map((die, i )=>(
-                        <motion.button transition={{ duration: 1.3 }} animate={{
-                            scale: [1, 2, 2, 1, 1],
-                            rotate: [0, 0, 270, 270, 0],
-                            
-                          }} className='dice-button' key={i} value={i} onClick={()=>{handleRoll(yourPlayerId, i)}}><Dice key={i} faceValue={die} /></motion.button>
-                    ))}
-                    </div>
-                     */}
-                        <div className='dice-container'>
-                        {/* {game.diceArrays[yourPlayerId].map((die, i )=>(
-                            <motion.button transition={{ duration: 1.3 }} animate={{
-                                scale: [1, 2, 2, 1, 1],
-                                rotate: [0, 0, 270, 270, 0],
-                                
-                            }} className='dice-button' key={i} value={i} onClick={()=>{handleRoll(yourPlayerId, i)}}><Dice faceValue={die} /></motion.button>))} */}
-
-                            <div className='roll-dice-button-container'>
-                                <div className="bottom-row grid-item">{yourPlayerId ? (
-                                <>
-                                
-                                    {/* {(game.currentPlayerIndex===Object.keys(players).indexOf(yourPlayerId)) &&
-                                        <div>
-                                            <motion.button className='handleRoll-button' whileHover={{ scale: 1.1 }}
-                                                whileTap={{ scale: 0.9 }} onClick={()=>{handleRollAll(yourPlayerId)}}>Roll Dice</motion.button>
-                                        </div>
-                                    } */}
-                            
-                                </>
-                            ) : (
-                                <>I am a spectator, so I don't have count</>
-                            )}</div>
-
-             
+                            <div className='dice-container'>
+                            {game.gameDice.map((die, i )=>(
+                                <Dice key={i} faceValue={die} />
+                            ))}
                             </div>
-                        </div>
-                        
+
+                    </div>
+
                     
                 </div>
 
-
-
-                
-
-                    
-             
-               
 
                 <div className='bottom-section'>
 
-                <motion.div  transition={{ duration: 1 }} animate={{x:0}} initial={{x:-150}}className={` ${playerIds[2] === yourPlayerId ? 'red-border' : ''}`}>
-                    {numPlayers > 2 ? (
-
-                        <div className=' player-flex player-3-name'>
-                            <b>{players[playerIds[1]].displayName}: {game?.diceCount[playerIds[1]]}</b>
-
-                        </div>
-                    ) : (
-                        <div className="player-3-name player-flex">
-                            <b>Waiting for player 3</b>
-                        </div>
-                    )}
-                </motion.div>
-
-
-                <motion.div  transition={{ duration: 1 }} animate={{x:0}} initial={{x:150}} className={` ${playerIds[3] === yourPlayerId ? 'red-border' : ''}`}>
-                    {numPlayers > 3 ? (
-
-                        <div className='player-flex player-4-name'>
-                           <b>{players[playerIds[1]].displayName}: {game?.diceCount[playerIds[1]]}</b>
-                        </div>
-                    ) : (
-                        <div className='player-flex player-4-name'>
-
-                           <b> Waiting for player 4</b>
-                        </div>
-                    )}
-                </motion.div>
-
-                    
                 </div>
-                <div className="dice-container-parent">
-                
-     
 
-                            {/*Controls area*/}
+                <div className="ice-container-parent">
+
+                    <div className={`bottom-right player-area grid-item ${playerIds[2] === yourPlayerId ? 'red-border' : ''}`}>
+                        {numPlayers > 2 ? (
+                            <div>
+                                <b>{players[playerIds[2]].displayName}: {game?.diceCount[playerIds[2]]}</b>
+                            </div>
+                        ) : (
+                            <div>
+                                Waiting for player 3
+                            </div>
+                        )}
+                    </div>
+
+                    <div className={`bottom-left player-area grid-item ${playerIds[3] === yourPlayerId ? 'red-border' : ''}`}>
+                        {numPlayers > 3 ? (
+                            <div>
+                                <b>{players[playerIds[3]].displayName}: {game?.diceCount[playerIds[3]]}</b>
+                            </div>
+                        ) : (
+                            <div>
+                                Waiting for player 4
+                            </div>
+                        )}
+                    </div>
+
+                    {/*Controls area*/}
                     {/*Roll Dice, Challenge, Give away*/}
                     <div className='roll-dice-button-container'>
                         <div className="bottom-row grid-item">{yourPlayerId ? (
                             <>
-                   {(game.currentPlayerIndex===Object.keys(players).indexOf(yourPlayerId)) &&
+
+                                {(game.currentPlayerIndex===Object.keys(players).indexOf(yourPlayerId)) &&
                                     <div>
                                         <button onClick={()=>{handleRollDice()}}>Roll Dice</button>
                                     </div>
-                                }     </>
-                                ) : (
-                                    <>I am a spectator, so I don't have count</>
-                                )}</div>
+                                }
 
+                            </>
+                        ) : (
+                            <>I am a spectator, so I don't have count</>
+                        )}</div>
 
-                    
+                    </div>
                 </div>
-        
-            </div>                           
-        
-         </div>
+
+
+        </div>
 
     );
 }
 
 export default GameZone;
 
-{/* //grid layout start by ChatGPT
+//grid layout start by ChatGPT
 //typescript editing Chat GPT
-// Image by <a href="https://www.freepik.com/free-vector/top-view-modern-restaurant-table-with-flat-design_2847028.htm#query=dining%20table%20top%20view&position=9&from_view=keyword&track=ais">Freepik</a> */}
+// Image by <a href="https://www.freepik.com/free-vector/top-view-modern-restaurant-table-with-flat-design_2847028.htm#query=dining%20table%20top%20view&position=9&from_view=keyword&track=ais">Freepik</a>
 {/* <a href="https://www.freepik.com/free-photo/faded-gray-wooden-textured-flooring-background_16246476.htm#page=2&query=flooring&position=8&from_view=search&track=sph">Image by rawpixel.com</a> on Freepik */}
 {/* <a href="https://www.freepik.com/free-vector/oak-wood-textured-design-background_16339756.htm#page=2&query=flooring&position=31&from_view=search&track=sph">Image by rawpixel.com</a> on Freepik */}
