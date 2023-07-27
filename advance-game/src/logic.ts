58287b6f03e71f63a83d4cee0b6cdb10d5200a4b
andrew <aihof757@gmail.com>
2023-07-27 19:47:00 -0400

    hey

--- advance-game/src/logic.ts
+++ advance-game/src/logic.ts
@@ -1,38 +1,38 @@
-import type { RuneClient } from "rune-games-sdk/multiplayer"
+// import type { RuneClient } from "rune-games-sdk/multiplayer"
 
-export interface GameState {
-  count: number
-}
+// export interface GameState {
+//   count: number
+// }
 
-type GameActions = {
-  increment: (params: { amount: number }) => void
-}
+// type GameActions = {
+//   increment: (params: { amount: number }) => void
+// }
 
-declare global {
-  const Rune: RuneClient<GameState, GameActions>
-}
+// declare global {
+//   const Rune: RuneClient<GameState, GameActions>
+// }
 
-export function getCount(game: GameState) {
-  return game.count
-}
+// export function getCount(game: GameState) {
+//   return game.count
+// }
 
-Rune.initLogic({
-  minPlayers: 1,
-  maxPlayers: 4,
-  setup: (): GameState => {
-    return { count: 0 }
-  },
-  actions: {
-    increment: ({ amount }, { game }) => {
-      game.count += amount
-    },
-  },
-  events: {
-    playerJoined: () => {
-      // Handle player joined
-    },
-    playerLeft() {
-      // Handle player left
-    },
-  },
-})
+// Rune.initLogic({
+//   minPlayers: 1,
+//   maxPlayers: 4,
+//   setup: (): GameState => {
+//     return { count: 0 }
+//   },
+//   actions: {
+//     increment: ({ amount }, { game }) => {
+//       game.count += amount
+//     },
+//   },
+//   events: {
+//     playerJoined: () => {
+//       // Handle player joined
+//     },
+//     playerLeft() {
+//       // Handle player left
+//     },
+//   },
+// })
