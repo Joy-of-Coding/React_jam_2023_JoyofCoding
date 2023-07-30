d278ed2a068d3ab049e7f8e985866af03acda707
Suzanne Atkinson <suzanneatkinson@gmail.com>
2023-07-30 01:55:31 -0400

    Removed old header

--- joy-game/src/components/GameZone.tsx
+++ joy-game/src/components/GameZone.tsx
@@ -38,10 +38,10 @@ const GameZone: React.FC<GameZoneProps> = ({game: game, players: players, yourPl
 
                
 
-                <motion.div className="display-player-name" transition={{ duration: 1.2 }} animate={{y:0}} initial={{y:-150}}   >
-                    
-                    <Header displayName={players[yourPlayerId].displayName} challengeCounter={0} challengeStatus={false} />
-                </motion.div>
+                {/*<motion.div className="display-player-name" transition={{ duration: 1.2 }} animate={{y:0}} initial={{y:-150}}   >*/}
+                {/*    */}
+                {/*    <Header displayName={players[yourPlayerId].displayName} challengeCounter={0} challengeStatus={false} />*/}
+                {/*</motion.div>*/}
 
 
                 <div className='top-section'>
