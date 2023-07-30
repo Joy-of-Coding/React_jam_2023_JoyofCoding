1726bc06696aa6463a92e46c66c6228c12792389
Suzanne Atkinson <suzanneatkinson@gmail.com>
2023-07-30 14:24:44 -0400

    Select Player UI skeleton (no props)

--- joy-game/src/components/SelectPlayer.tsx
+++ joy-game/src/components/SelectPlayer.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import {AnimatePresence, motion} from "framer-motion";
+import './SelectPlayer.css'
 
 
 interface SelectPlayerProps {
     closePopup: () => void;
+}
+
+const handleClick = ({playerId}) => {
+    console.log("User Id: ", playerId)
 }
 
 const SelectPlayer: React.FC<SelectPlayerProps> = ({ closePopup }) => {
@@ -13,7 +18,12 @@ const SelectPlayer: React.FC<SelectPlayerProps> = ({ closePopup }) => {
             <AnimatePresence>
                 <motion.div transition={{ duration: .5 }} animate={{x:0}}initial={{x:250}}
                 className="popup-body">
-                    <h2>Choose who to share your gifts with! </h2>
+                    <span><b>To whom would you like to gift a random number of dice?</b></span>
+                    <div className='playerSelect'>
+                        <div className='opponent' value={"1"} onClick={()=> (handleClick({playerId: "1"}))}>1</div>
+                        <div className='opponent' value={"2"} onClick={()=> (handleClick({playerId: "2"}))}>2</div>
+                        <div className='opponent' value={"3"} onClick={()=> (handleClick({playerId: "3"}))}>3</div>
+                    </div>
                     <button onClick={closePopup}>Close</button>
                 </motion.div>
             </AnimatePresence>
