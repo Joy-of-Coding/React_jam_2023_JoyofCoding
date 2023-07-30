bc6607d8e15c9eede99b25b2b77eb322fbf5f593
Joel Davila <joeldavila@joels-mbp.lan>
2023-07-30 00:00:44 -0400

    added npm howler library, sound play on first screen touch

--- joy-game/src/components/Table.tsx
+++ joy-game/src/components/Table.tsx
@@ -18,7 +18,8 @@ interface TableProps {
 
 const Table: React.FC<TableProps> = ({ game, playerId, playerIds }) => {
   const currentPlayerId = playerIds.indexOf(playerId);
-    
+
+  
   const handleDiceClick = (faceValue: number, playerId: string | undefined, i: number, playerIds: (string | undefined)[]) => {
       //Trying to disable clicks by player
       
@@ -38,7 +39,8 @@ const Table: React.FC<TableProps> = ({ game, playerId, playerIds }) => {
             const nextPlayerId = playerIds[(currentPlayerId + 1) % Object.keys(playerIds).length];
            // console.log(playerId)
            // console.log([nextPlayerId])
-            window.navigator.vibrate([100]);
+           
+            // window.navigator.vibrate([100]);
             Rune.actions.updateDiceCount({playerId: nextPlayerId, amount: 1})
             Rune.actions.updateDiceCount({playerId: playerId, amount: -1})
             Rune.actions.adjustGameDice({index: i})
@@ -49,6 +51,8 @@ const Table: React.FC<TableProps> = ({ game, playerId, playerIds }) => {
 
 
     return (
+
+
         <div className='middle-section'>
           <div  className='dice-container'>
             {game.gameDice.map((die, i) => (
