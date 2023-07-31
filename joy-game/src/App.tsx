af92ed11847b7192e8d3f06c0bd4634021b94290
KatrinaWright <katrina.elise.wright@gmail.com>
2023-07-31 11:43:27 -0400

    Submission!

--- joy-game/src/App.tsx
+++ joy-game/src/App.tsx
@@ -5,6 +5,10 @@ import { GameState } from "./logic.ts"
 import GameZone from "./components/GameZone";
 import ReactHowler from 'react-howler'
 import Music from "./assets/sounds/ES_Twinkle Mind - Stationary Sign.mp3"
+//import pop from "./assets/sounds/pop.mp3"
+
+//const popSound = new Audio(pop)
+//const popBallons (or swallow, or surprose)  = new Audio("relative path to sound file")
 
 function App() {
   const [game, setGame] = useState<GameState>()
@@ -13,10 +17,13 @@ function App() {
 
   useEffect(() => {
     Rune.initClient({
-          onChange: ({ newGame, players, yourPlayerId }) => {
+          onChange: ({ newGame, players, yourPlayerId,  }) => {
               setGame(newGame)
               setPlayers(players)
               setYourPlayerId(yourPlayerId)
+              // if (action && action.action === "popBalloons") {
+              //   popSound.play()
+              // }
 
           },
         }
