1962f1bf09c495c5ffc6772bd0f5a7339e567c51
Joel Davila <joeldavila@joels-mbp.lan>
2023-08-05 23:30:43 -0400

    added credit component to show credits on popup

---
+++ joy-game/src/components/Credits.tsx
@@ -0,0 +1,55 @@
+
+
+import React from 'react';
+import './HelpPopup.css';
+import { motion} from "framer-motion"
+import "./Credits.css"
+
+
+interface Credits {
+  closePopup: () => void;
+}
+
+export const Credits: React.FC<Credits> = ({ closePopup }) => {
+  return (
+
+  
+        
+    <div className="credits-container">
+      
+      <motion.div transition={{ duration: .5 }} animate={{x:0,opacity:1}}initial={{x:250}}
+
+      className="popup-credit">
+
+        <div className='credits'>
+
+
+        <h1>Development Team</h1>
+
+           
+            <motion.p transition={{ duration: .5, delay:.5 }} animate={{x:0,opacity: 100}}initial={{x:'-100vw',opacity:0}}>Project Lead: Katrina Wright</motion.p> 
+            <motion.p transition={{ duration: .5, delay:1  }} animate={{x:0,opacity: 100}}initial={{x:'100vw',opacity:0}}>Head Engineeer: Suzanne</motion.p> 
+            <motion.p transition={{ duration: .5, delay:1.5  }} animate={{x:0,opacity: 100}}initial={{x:'-100vw',opacity:0}}>Head UI/UX Engineer: Joel Davila</motion.p> 
+            <motion.p transition={{ duration: .5, delay:2  }} animate={{x:0,opacity: 100}}initial={{x:'100vw',opacity:0}}>Artist: Kassandra "Chase" Tramel</motion.p> 
+            <motion.p transition={{ duration: .5, delay:2.5  }} animate={{x:0,opacity: 100}}initial={{x:'-100vw',opacity:0}}>Project Consultant: Dr. Dana</motion.p> 
+
+            <h2>Additional Developers: </h2>
+
+            <motion.p transition={{ duration: .5, delay:3  }} animate={{x:0,opacity: 100}}initial={{x:'100vw',opacity:0}}>Alyssa</motion.p> 
+
+
+
+
+            <motion.button  whileHover={{ scale: 1.1 }} onClick={closePopup}>Close</motion.button>
+
+        </div>
+      
+        {/* <button onClick={closePopup}>Credits</button> to add credits */}
+      </motion.div>
+      
+    </div>
+   
+  );
+};
+
+export default Credits;
--- joy-game/src/components/HelpPopup.tsx
+++ joy-game/src/components/HelpPopup.tsx
@@ -1,8 +1,9 @@
 
 
-import React from 'react';
+import React, { useState } from 'react';
 import './HelpPopup.css';
 import { motion, AnimatePresence} from "framer-motion"
+import { Credits } from './Credits';
 
 
 interface HelpPopupProps {
@@ -10,8 +11,10 @@ interface HelpPopupProps {
 }
 
 export const HelpPopup: React.FC<HelpPopupProps> = ({ closePopup }) => {
+  const [open, setOpen] = useState(false);
+  
   return (
-    <div className="popup-container"onClick={closePopup}>
+    <div className="popup-container">
       <AnimatePresence>
       <motion.div transition={{ duration: .5 }} animate={{x:0}}initial={{x:250}}
 
@@ -21,10 +24,17 @@ export const HelpPopup: React.FC<HelpPopupProps> = ({ closePopup }) => {
           <p>Get rid of all of your dice to win!</p>
           <h2>How To Play</h2>
           <p>Click the green "roll" button on your turn & tap the dice:</p>
-          <p>1: Pop balloons to remove them <p>2: Gifts have a random amount of dice, choose who gets them </p>3: Share the cake with everyone else </p>
+          <p>1: Pop balloons to remove them</p> 
+          <p>2: Gifts have a random amount of dice, choose who gets them </p>
+          <p>3: Share the cake with everyone else </p>
           <p>When only confetti is left, click the red "end turn" button.</p>
-          <p>Made for fun by students of Emily Hill's Joy of Coding Academy - visit JoyofCoding.Academy for program details</p>
-        <button onClick={closePopup}>Close</button>
+          {/* <p>Made for fun by students of Emily Hill's Joy of Coding Academy - visit JoyofCoding.Academy for program details</p> */}
+
+        <div>
+          {open && <Credits closePopup={() => setOpen(false)} />}
+          <motion.button  whileHover={{ scale: 1.1 }}  onClick={() => setOpen(true)}><b>Credits</b></motion.button>
+        </div>      
+        <motion.button  whileHover={{ scale: 1.1 }} onClick={closePopup}>Close</motion.button>
         {/* <button onClick={closePopup}>Credits</button> to add credits */}
       </motion.div>
       </AnimatePresence>
