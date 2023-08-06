79c8baf3db32030934f61a835cd4002b3a4b3039
KatrinaWright <katrina.elise.wright@gmail.com>
2023-08-06 10:21:09 -0400

    endless bug fix and credits

--- joy-game/src/components/SelectPlayer.tsx
+++ joy-game/src/components/SelectPlayer.tsx
@@ -31,7 +31,7 @@ const SelectPlayer: React.FC<SelectPlayerProps> = ({ yourPlayerId, playerIds, cl
             <AnimatePresence>
                 <motion.div transition={{ duration: .5 }} animate={{x:0}}initial={{x:250}}
                 className="popup-body">
-                    <span><b>To whom would you like to gift a random number of dice (including taking one away)?</b></span>
+                    <span><b>To whom would you like to gift a random number of dice (including taking some away)?</b></span>
 
                     <div className='playerSelect'>
 
