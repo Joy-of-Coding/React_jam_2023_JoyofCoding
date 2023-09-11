3e9af4f92ba86dc5feebd2b343ae472a5b022dd4
Suzanne Atkinson <suzanneatkinson@gmail.com>
2023-09-11 13:34:02 -0400

    removed component not used

--- dice-roller/src/Components/Dice.js
+++ dice-roller/src/Components/Dice.js
@@ -1,10 +1,9 @@
 import './Dice.css'
-import Icosahedron from "./polyhedrons/Icosahedron";
 
 
 const Dice = ({value}) => {
     return (
-        value === 20 ? <Icosahedron/> :
+      
 
             <div className="dice">
                 {value}
