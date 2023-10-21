485cc4dbc26f1a0ce9b2069a8bf6ec47c9d5f90e
Suzanne Atkinson <suzanneatkinson@gmail.com>
2023-10-20 20:52:09 -0400

    cleaned up a small TS issue

--- battle-sweep/src/logic.ts
+++ battle-sweep/src/logic.ts
@@ -19,12 +19,29 @@ export function getCount(game: GameState) {
 Rune.initLogic({
   minPlayers: 1,
   maxPlayers: 4,
-  setup: (): GameState => {
-    return { count: 0 }
+  setup: (allPlayerIds): GameState => {
+    const game = {
+      count: null,
+      scores: {}
+    }
+    for (const playerId of allPlayerIds) {
+      game.scores[playerId] = 0
+    }
+    game.count = 0
+    return game
+    // return { count: 0 }
   },
   actions: {
     increment: ({ amount }, { game }) => {
       game.count += amount
     },
   },
+  events: {
+    playerJoined: (playerId, {game}) => {
+    game.scores[playerId] = 0
+   },
+    playerLeft:(playerId, {game}) => {
+       delete game.scores[playerId]
+    },
+  }
 })
