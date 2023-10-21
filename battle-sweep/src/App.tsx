bab6d7217092380736adfc4d0f8fccc06cf747b8
Patrick <93539953+rubybeaches@users.noreply.github.com>
2023-10-20 20:02:20 -0500

    updating app with new board import

--- battle-sweep/src/App.tsx
+++ battle-sweep/src/App.tsx
@@ -1,48 +1,24 @@
-import { useEffect, useState } from "react"
-import reactLogo from "./assets/rune.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
-import { GameState } from "./logic.ts"
+import { useEffect, useState } from "react";
+import { GameState } from "./logic.ts";
+import Board from "./components/Board.tsx";
+import "./App.css";
 
 function App() {
-  const [game, setGame] = useState<GameState>()
+  const [game, setGame] = useState<GameState>();
   useEffect(() => {
     Rune.initClient({
       onChange: ({ game }) => {
-        setGame(game)
+        setGame(game);
       },
-    })
-  }, [])
+    });
+  }, []);
 
   if (!game) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://developers.rune.ai" target="_blank">
-          <img src={reactLogo} className="logo rune" alt="Rune logo" />
-        </a>
-      </div>
-      <h1>Vite + Rune</h1>
-      <div className="card">
-        <button onClick={() => Rune.actions.increment({ amount: 1 })}>
-          count is {game.count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> or <code>src/logic.ts</code> and save to
-          test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and Rune logos to learn more
-      </p>
-    </>
-  )
+  // <button onClick={() => Rune.actions.increment({ amount: 1 })}></button>
+  return <Board width={9} height={9} />;
 }
 
-export default App
+export default App;
