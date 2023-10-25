9da9f675e52aaa964af678b870641e5962095593
Suzanne Atkinson <suzanneatkinson@gmail.com>
2023-10-25 03:35:52 -0400

    timer showing twice instead of once for everyone.

--- battle-sweep/src/App.tsx
+++ battle-sweep/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { GameState } from "./logic.ts";
 import type { Players, PlayerId } from "rune-games-sdk/multiplayer";
 import Board from "./components/Board.tsx";
@@ -7,7 +7,7 @@ import Player from "./components/Player.tsx";
 import Controls from "./components/Controls.tsx";
 import { HelpPopup } from "./components/HelpPopup.tsx";
 import { motion } from "framer-motion";
-import Timer from "./components/Timer.tsx"; // Import the Timer component with correct casing
+import Timer from "./components/Timer.tsx";
 
 
 function App() {
@@ -15,15 +15,9 @@ function App() {
   const [players, setPlayers] = useState<Players>({});
   const [yourPlayerId, setYourPlayerId] = useState<PlayerId>();
   const playerIds = Object.keys(players);
-  const [open, setOpen] = useState(false);
-  const [useFlag, setUseFlag] = useState(false);
   const timerRef = useRef<number>(0);
-  const [useFlag, setUseFlag] = useState(false); // Add useFlag state
-  const [open, setOpen] = useState(false); // Add open state
-  // const [timerDuration, setTimerDuration] = useState<number>(10); // Define and initialize the timer duration
-  // const [gameFinished, setGameFinished] = useState(false); // Declare and initialize gameFinished
-
-  // console.log(timerDuration)
+  const [useFlag, setUseFlag] = useState(false);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     Rune.initClient({
@@ -73,56 +67,53 @@ function App() {
 
   const toggleFlagState = () => {
     setUseFlag(!useFlag);
-  const toggleFlag = () => {
-    setUseFlag((prevUseFlag) => !prevUseFlag);
   };
 
   if (!game) {
     return <div>Loading...</div>;
   }
 
-
-
   return (
-    <>
-      {playerIds.map((id) => (
-        <>
-          <Player
-            key={id + "-player"}
-            display={game.onboarding ? id == yourPlayerId : id != yourPlayerId}
-            players={players}
-            playerId={id}
-            game={game}
-          />
-          <Board
-            key={id}
-            onPress={handleTilePress}
-            onLongPress={handleLongTilePress}
-            display={game.onboarding ? id == yourPlayerId : id != yourPlayerId}
-            board={game.playerState[`${id}`].board}
-          />
-        </>
-      ))}
-      {/*//Onboard timer*/}
-      <Timer
-          key={"-onboardtimer"}
-          initialTime={game.onBoardTimer}
-          endFunction={Rune.actions.swap}
-          // setTimerDuration={setTimerDuration}
-      />
-
-      <Controls onboarding={game.onboarding} toggleFlag={toggleFlagState} useFlag={useFlag}/>
-      <div>
-        {open && <HelpPopup closePopup={() => setOpen(false)} />}
-        <motion.button
-          whileHover={{ scale: 1.1 }}
-          className="helpButton"
-          onClick={() => setOpen(true)}
-        >
-          <b>Info</b>
-        </motion.button>
-      </div>
-    </>
+      <>
+        {playerIds.map((id) => (
+            <React.Fragment key={id+ "-players"}>
+              <Player
+                  key={id + "-player"}
+                  display={game.onboarding ? id === yourPlayerId : id !== yourPlayerId}
+                  players={players}
+                  playerId={id}
+                  game={game}
+              />
+              <Board
+                  key={id}
+                  onPress={handleTilePress}
+                  onLongPress={handleLongTilePress}
+                  display={game.onboarding ? id === yourPlayerId : id !== yourPlayerId}
+                  board={game.playerState[id].board}
+              />
+            </React.Fragment>
+        ))}
+        <Timer
+            key="-onboardtimer"
+            initialTime={game.onBoardTimer}
+            endFunction={Rune.actions.swap}
+        />
+        <Controls
+            onboarding={game.onboarding}
+            toggleFlag={toggleFlagState}
+            useFlag={useFlag}
+        />
+        <div>
+          {open && <HelpPopup closePopup={() => setOpen(false)} />}
+          <motion.button
+              whileHover={{ scale: 1.1 }}
+              className="helpButton"
+              onClick={() => setOpen(true)}
+          >
+            <b>Info</b>
+          </motion.button>
+        </div>
+      </>
   );
 }
 
