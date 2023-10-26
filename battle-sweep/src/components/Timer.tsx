6fcc38366a81d9abce2167ab79d52b21f1974493
Suzanne Atkinson <suzanneatkinson@gmail.com>
2023-10-26 10:15:11 -0400

    timer compiles, fixed GIF import

--- battle-sweep/src/components/Timer.tsx
+++ battle-sweep/src/components/Timer.tsx
@@ -2,51 +2,22 @@ import React, { useState, useEffect } from "react";
 import { endGame } from "../logic.ts";
 import { GameState } from "../helper/Types.ts"
 import "./Timer.css"
-import "./assets/Explosions/fire_ball_side_medium_explosion.gif"
+import explosionGif from "../assets/Explosions/fire_ball_side_medium/fireball_side_medium_explode.gif"
 
 interface TimerProps {
   game: GameState;
   initialTime: number;
-  onTimerEnd: () => void;
-  // playerWin: string; // Add this prop
-  // playerLose: string; // Add this prop
   endGame: (game: GameState) => void; // Add this prop
 
 }
 
-function Timer({ game, initialTime, onTimerEnd }: TimerProps) {
-  const [timer, setTimer] = useState<number>(initialTime);
+function Timer({ game, initialTime }: TimerProps) {
 
-  // useEffect(() => {
-  //   if (timer > 0) {
-  //     const interval = setInterval(() => {
-  //       setTimer((prevTimer) => {
-  //         if (prevTimer > 0) {
-  //           return prevTimer - 1;
-  //         } else {
-  //           clearInterval(interval);
-  //           onTimerEnd(); // Call the onTimerEnd function when the timer reaches zero
-  //           endGame(playerWin, playerLose); // Call endGame function to end the game
-  //           return 0; // Ensure the timer remains at 0
-  //         }
-  //       });
-  //     }, 1000);
-
-  //     // Clean up the timer when the component unmounts or when the timer reaches zero
-  //     return () => {
-  //       clearInterval(interval);
-  //     };
-  //   } else {
-  //     console.log("Time's Up")
-  //   }
-  // }, [onTimerEnd, playerLose, playerWin, timer]);
-
-  // useEffect(() => {
-  //   if (game.onBoardTimer == 0) {
-  //     // Rune.actions.timerEnd()
-  //     <img src="./assets/Explosions/fire_ball_side_medium/fireball_side_medium_explosion.gif"/>
-  //   }
-  // }, [game])
+  useEffect(() => {
+    if (game.onBoardTimer == 0) {
+      // Action to take on Timer End
+    }
+  }, [game])
 
   return (
     game.onBoardTimer > 0 ? 
@@ -54,7 +25,7 @@ function Timer({ game, initialTime, onTimerEnd }: TimerProps) {
         Timer: {game.onBoardTimer} second(s)
       </div> : 
       <div>
-        <img src="./assets/Explosions/fire_ball_side_medium/fireball_side_medium_explosion.gif"/>
+        <img src={explosionGif}/>
         <p>Game Over</p>
       </div>
   );
