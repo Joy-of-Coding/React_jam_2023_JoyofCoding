336507e7a1f9e43cf6f7de927ba6d0aeb912c43c
Suzanne Atkinson <suzanneatkinson@gmail.com>
2023-10-26 00:48:10 -0400

    Update Config.tsx
    removed unused code never in production

--- battle-sweep/src/components/Config.tsx
+++ battle-sweep/src/components/Config.tsx
@@ -38,34 +38,6 @@ export const Config: React.FC<ConfigProps> = ({ closePopup, game }) => {
 
     </div>
     <div>
-      
-    {/* <button className="button" onClick={() => Rune.actions.userSetBombCount(5)}>
-          Swap Boards
-        </button> */}
-    </div>
-
-
-    {/* <script>
-        function saveConfig() {
-            const bombCount = parseInt(document.getElementById("bombCount").value);
-            const gameTimer = parseInt(document.getElementById("gameTimer").value);
-            const turnTimer = parseInt(document.getElementById("turnTimer").value);
-            const fieldSize = document.getElementById("fieldSize").value;
-
-            // You can use these values in your JavaScript game code
-            console.log("Max Lives: " + maxLives);
-            console.log("Bomb Count: " + bombCount);
-            console.log("Game Timer: " + gameTimer + " seconds");
-            console.log("Turn Timer: " + turnTimer + " seconds");
-            console.log("Field Size: " + fieldSize);
-        {'}'}
-}
-    </script> */}
-
-        {/* <div>
-          {open && <Credits closePopup={() => setOpen(false)} />}
-          <motion.button  whileHover={{ scale: 1.1 }}  onClick={() => setOpen(true)}><b>Credits</b></motion.button>
-        </div>       */}
         <motion.button  whileHover={{ scale: 1.1 }} onClick={closePopup}>Close</motion.button>
       </motion.div>
       </AnimatePresence>
