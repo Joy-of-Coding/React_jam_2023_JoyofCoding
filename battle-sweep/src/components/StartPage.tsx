bab4685bbacc1035cfde437d0f96ab576f895f7f
andrew <aihof757@gmail.com>
2023-10-26 14:30:33 -0400

    setting

---
+++ battle-sweep/src/components/StartPage.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./StartPage.css" 
+import './StartPage.css';
+
+const StartPage: React.FC = () => {
+    
+  return (
+    <div>
+      <h1>Start Game</h1>
+      
+      <h1>Settings</h1>
+      <h1>Help</h1>
+      {/* Add your Start Page content here */}
+    </div>
+
+  );
+};
+
+export default StartPage;
+
+
+  
