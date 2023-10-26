5f6c035dec77b8bbc4a146a15bd52e02111911d7
rubybeaches <patrick.mclain.jr@gmail.com>
2023-10-26 13:45:48 -0500

    Updating timer

--- battle-sweep/src/helper/Types.ts
+++ battle-sweep/src/helper/Types.ts
@@ -11,11 +11,14 @@ export interface TileProp {
   }
   
   export interface GameState {
-    onBoardTimer: number,
-    gameStart: number,
     playerIds: PlayerId[],
     onboarding: boolean,
     isGameOver: boolean,
+    onBoardTime: number,
+    playTime: number,
+    gameTimer: number,
+    timeElapsed: number,
+    stopTimer: boolean,
     setBombs: number,
     baselineScore: number,
     playerState: {
