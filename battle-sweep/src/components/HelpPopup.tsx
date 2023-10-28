eb5e6d4c1f82983369d270a6a567ad1e5e2e6c57
kianu <thomasulee@gmail.com>
2023-10-27 23:12:42 -0400

    update credits page with accurate information. ALL DEVS, add your preferred name and title here for the update!

--- battle-sweep/src/components/HelpPopup.tsx
+++ battle-sweep/src/components/HelpPopup.tsx
@@ -19,9 +19,16 @@ export const HelpPopup: React.FC<HelpPopupProps> = ({ closePopup }) => {
       className="popup-body">
 
       <h2>Battle Sweep</h2>
-          <p className="game-synopsis">Battle Sweeper is an exciting and strategic two-player board game that combines the classic elements of Minesweeper and Battleship. In this game, players become naval commanders who must strategically deploy hidden mines on their opponent's grid while also skillfully navigating their own minefield.</p>
+          <p>Battle Sweep is an exciting and strategic two-player game that combines the classic elements of Minesweeper and Battleship. In this game, players must strategically deploy hidden mines on their opponent's grid while also skillfully navigating their own minefield.</p>
           <h2>How To Play</h2>
-          <p>Classic Minesweeper with a twist! You place mines then send the field to your opponent for them to sweep. </p>
+          <p>
+            <ul>
+              <li><b>Setting up the Battlefield:</b> At the start of the game, strategically place a limited number of mines in secret locations on the opposing player's grid.</li>
+              <li><b>Uncovering Tiles:</b> Click (or tap on touchscreens) on a tile to uncover it. If a tile contains an opponent's mine, it detonates and your game is over.</li>
+              <li><b>Flagging Mines:</b> The objective of the game is to uncover as many mines as you can.  To uncover a mine you must first flag it by clicking on the flag button and then clicking the tile containg the mine.  Deselect the flag button and click the same tile to uncover the mine.</li>
+              <li><b>Winning:</b> To win, you must successfully uncover more mines than your opponent.</li>
+            </ul>  
+          </p>
 
         <div>
           {open && <Credits closePopup={() => setOpen(false)} />}
