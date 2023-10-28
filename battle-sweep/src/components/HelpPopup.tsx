b58647699fc104f9c2f57ebc4bf005342a749eb7
dhughneil <dhughneil@gmail.com>
2023-10-28 13:52:10 -0600

    Fixed typo and improved formatting.

--- battle-sweep/src/components/HelpPopup.tsx
+++ battle-sweep/src/components/HelpPopup.tsx
@@ -19,13 +19,14 @@ export const HelpPopup: React.FC<HelpPopupProps> = ({ closePopup }) => {
       className="popup-body">
 
       <h2>Capture the Crabs</h2>
-          <p>Capture the Crabs is an exciting and strategic two-player game that combines the classic elements of Minesweeper and Battleship. In this game, players must strategically deploy hidden crabs on their opponent's grid while also skillfully navigating their own lava field.</p>
+          <p>Capture the Crabs is an exciting and strategic two-player game that combines the classic elements of Minesweeper and Battleship. In this game, players must strategically deploy <b>hidden crabs</b> on their opponent's grid while also skillfully navigating their own lava field.</p>
           <h2>How To Play</h2>
           <p>
             <ul>
               <li><b>Setting up the lava field:</b> At the start of the game, strategically place a limited number of crabs in secret locations on the opposing player's grid.</li>
               <li><b>Uncovering Tiles:</b> Click (or tap on touchscreens) on a tile to uncover it. If a tile contains an opponent's crab, it bites with its lava claw and your game is over.</li>
-              <li><b>Setting traps:</b> The objective of the game is to trap as many crabs as you can.  To trap a crab you must first place a trap it by clicking on the trap button and then clicking the tile of the lava field where the crab is.  Deselect the trap button and click the same tile to trigger the trap and catch the crab.</li>
+              <li><b>Setting traps:</b> The objective of the game is to trap as many crabs as you can.  To trap a crab you must first place a trap by clicking on the trap button and then clicking the tile of the lava field where you believe the crab is. </li>
+              <li><b>Trap the crabs: </b>Deselect the trap button and click the same tile to trigger the trap and catch the crab.</li>
               <li><b>Winning:</b> To win, you must successfully trap more crabs than your opponent.</li>
             </ul>  
           </p>
