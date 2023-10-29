96e26dce0dcbf094ab5ff4e7c0e89b261e8c952e
rubybeaches <patrick.mclain.jr@gmail.com>
2023-10-29 00:54:46 -0500

    remove styledbaord

--- battle-sweep/src/components/StyledBoard.tsx
+++
@@ -1,5 +0,0 @@
-export const StyledBoard = styled.div`
-    display: grid;
-    grid-template-rows: repeat(${props => props.height}, calc(25vw / ${props => props.width}));
-    grid-template-columns: repeat(${props => props.width}, 1fr);
-`
